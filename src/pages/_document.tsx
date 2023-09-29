5c9feff336a3006e7dca267dc1091e0d1739b101
Vadim Bezdushny <vadim.bezdushniy@snowstormtech.com>
2023-09-29 14:10:57 +0300

    meta domain check

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useRouter } from 'next/router';
 
 export default function Document() {
   const origin =
@@ -10,13 +9,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="facebook-domain-verification" content="7qoi7pzstxcgc2tuelu71cx569y3ez" />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="og:title" />
-        <meta property="og:description" content="og:description" />
-        <meta property="og:image" content="https://ik.imagekit.io/vadimfacebooktest/a237fc26-287b-460c-afab-0e69adc81543.jpeg?tr=w-1200,h-630" />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,36 @@
-import { Inter } from 'next/font/google'
-import { GetStaticProps } from 'next'
+import { Inter } from "next/font/google";
+import { GetServerSideProps, GetStaticProps } from "next";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const getStaticProps: GetStaticProps = () => {
-  return {
-    props: {},
-    revalidate: 10,
-  }
-}
+export const getServerSideProps = (async (context) => {
+  const tr = String(context.query.tr);
+  return { props: { tr } };
+}) satisfies GetServerSideProps<{
+  tr: string;
+}>;
 
-export default function Home() {
+export default function Home({ tr }: { tr: string }) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
-    >
-      <h1 className='text-center'>FACEBOOK TEST PAGE</h1>
-    </main>
-  )
+    <>
+      <Head>
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="og:title" />
+        <meta property="og:description" content="og:description" />
+        <meta
+          property="og:image"
+          content={`https://ik.imagekit.io/vadimfacebooktest/a237fc26-287b-460c-afab-0e69adc81543.jpeg?${tr}`}
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
+      >
+        <h1 className="text-center">FACEBOOK TEST PAGE</h1>
+      </main>
+    </>
+  );
 }
